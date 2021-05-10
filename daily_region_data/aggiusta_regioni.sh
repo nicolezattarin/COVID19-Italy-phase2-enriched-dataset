19884b50eee44b6847bae052bdd5168da3f7d142
DaveParm <kiogre.is.a.noob@gmail.com>
2021-05-10 10:14:37 +0200

    update

---
+++ daily_region_data/aggiusta_regioni.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Reitera il singolo aggiusta regione per tutte le regioni.
+rm *_aggiustata.csv
+./aggiusta_regione Abruzzo
+./aggiusta_regione Basilicata
+./aggiusta_regione Calabria
+./aggiusta_regione Campania
+./aggiusta_regione Emilia-Romagna
+./aggiusta_regione Friuli_Venezia_Giulia
+./aggiusta_regione Lazio
+./aggiusta_regione Liguria
+./aggiusta_regione Lombardia
+./aggiusta_regione Marche
+./aggiusta_regione Molise
+./aggiusta_regione P.A.Bolzano
+./aggiusta_regione P.A.Trento
+./aggiusta_regione Piemonte
+./aggiusta_regione Puglia
+./aggiusta_regione Sardegna
+./aggiusta_regione Sicilia
+./aggiusta_regione Toscana
+./aggiusta_regione Umbria
+./aggiusta_regione Valle_Aosta
+./aggiusta_regione Veneto
