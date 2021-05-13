eb622fe8ff41476682db156bf52536dd1b3843f7
DaveParm <kiogre.is.a.noob@gmail.com>
2021-05-13 14:47:41 +0200

    update

---
+++ daily_region_data/scripts/fix_all_regions.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# This bash script simply reiterates the single fix_region script for all regions.
+rm fixed_*.csv
+./fix_region Abruzzo
+./fix_region Basilicata
+./fix_region Calabria
+./fix_region Campania
+./fix_region Emilia-Romagna
+./fix_region Friuli_Venezia_Giulia
+./fix_region Lazio
+./fix_region Liguria
+./fix_region Lombardia
+./fix_region Marche
+./fix_region Molise
+./fix_region P.A.Bolzano
+./fix_region P.A.Trento
+./fix_region Piemonte
+./fix_region Puglia
+./fix_region Sardegna
+./fix_region Sicilia
+./fix_region Toscana
+./fix_region Umbria
+./fix_region Valle_Aosta
+./fix_region Veneto
